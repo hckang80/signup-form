afc18eebeda652c66a837a36b1c042f0bcd1fc0d
hckang <hckang80@gmail.com>
2018-05-16 01:12:14 +0900

    Create Instagram account input field

---
+++ src/form/InstagramAccount.tsx
@@ -0,0 +1,40 @@
+import cc from "classcat";
+import * as m from "mithril";
+import BaseControl, { IBaseControl } from "./BaseControl";
+import Value from "./Value";
+
+interface IInstagramAccount extends IBaseControl<string> {}
+
+class InstagramAccount extends BaseControl<IInstagramAccount, string> {
+  private account: string;
+
+  public oninit(vnode: m.CVnode<IInstagramAccount>) {
+    super.oninit(vnode);
+    this.oninput(this.value());
+  }
+
+  protected controls() {
+    return (
+      <div class={cc(["form-control", { "is-invalid": !this.value.isValid() }])}>
+        <div class={cc("row")}>
+          <span class="col-sm-4">http://www.instagram.com/</span>
+          <span class="col-sm-8">
+          <input
+            oninput={m.withAttr("value", value => this.oninput(value))}
+            type="text"
+            class={cc("form-control")}
+            value={this.account}
+          />
+          </span>
+        </div>
+      </div>
+    );
+  }
+
+  private oninput(value: string) {
+    this.value(value);
+    this.account = value;
+  }
+}
+
+export default InstagramAccount;
